eea0188f1f6905caa1772b645edcf0f7fba942d3
Jivesh <jivesh2231@gmail.com>
2026-01-22 09:36:52 +0530

    "--- Delete chapter 1 source files"

--- src/ch01/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Modern C++ environment is working.\n";
-    return 0;
-}
--- src/ch01/source.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-
-int main(){
-    std::cout << "Hello World.\nThis is the program for:\nString literals, Escape Sequence, Std:: --> standard stream";
-}
