e6585de23f66a6212d8ad58882765ab3a9cf4f8d
Jivesh <jivesh2231@gmail.com>
2026-01-22 05:53:27 +0530

    chapter 5 --> Types(problem 1)

---
+++ src/ch05/p1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int main(){
+    bool a{true};
+    char b = 'A';
+    int c = 65;
+    double d = 65.0;
+
+    std::cout << "bool = " << a << "char = " << b << "int = " << c << "double = " << d;
+}
