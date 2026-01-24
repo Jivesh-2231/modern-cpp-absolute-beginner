2cb461ce75833a4ce26a2916d4dccf787dda886e
Jivesh <jivesh2231@gmail.com>
2026-01-24 06:29:22 +0530

    Chapter 10 --> Arrays

---
+++ src/ch10/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int main (){
+    {
+        //Problem - 1
+        int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+        std::cout << arr[9] << arr[8] << arr[7] << arr[6] << arr[5] << arr[4] << arr[3] << arr[2] << arr[1] << arr[0];
+    }
+    std::cout << "\n\n";
+}
